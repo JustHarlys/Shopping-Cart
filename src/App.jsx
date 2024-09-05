85aef3f685efb80fdd080b6bae844bcc1385a2d5
harly <harlysmc@gmail.com>
2024-09-05 14:54:19 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { data as productosIniciales} from './mocks/products.js'
+import Products from './Components/Products.jsx'
+import { useState } from 'react'
+import { Header } from './Components/Header.jsx'
+
+let initialProducts = productosIniciales.products
+
+function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+    return (
+      product.price >= filters.minPrice && 
+      (
+        filters.category === 'all' ||
+        product.category === filters.category
+      )
+    )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
+  return (
+    <>
+      <Header />
+      <Products products={filteredProducts}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Filters.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Filters() {
+  return (
+    <section className='filters'>
+      <div>
+        <label htmlFor="price">Price</label>
+        <input 
+        type="range" 
+        id='price'
+        min='0'
+        max='1000'
+        />
+      </div>
+    </section>
+  )
+}
+
+export default Filters
