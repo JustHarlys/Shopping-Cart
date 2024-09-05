0d6eb71d372b569d5a26e6928ae512131ee142c9
harly <harlysmc@gmail.com>
2024-09-05 15:15:01 -0400

    Terminado el filtro visual

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Filters.css'
 
 function Filters() {
+
+  const [minPrice, setMinPrice] = useState(0)
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value)
+  }
+
   return (
     <section className='filters'>
       <div>
-        <label htmlFor="price">Price</label>
+        <label htmlFor="price">Precio a partir de: </label>
         <input 
         type="range" 
         id='price'
         min='0'
         max='1000'
+        onChange={handleChangeMinPrice}
         />
+        <span>${minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor="Category">
+          <select name="" id="category">
+            <option value="all">Todas</option>
+            <option value="laptops">Portatiles</option>
+            <option value="smartphones">Celulares</option>
+          </select>
+        </label>
       </div>
     </section>
   )
